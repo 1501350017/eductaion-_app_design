1ff00766ed78913e2df57a211da2934baf7a37a8
Hitesha <36784043+1501350017@users.noreply.github.com>
2020-07-11 09:56:14 -0700

    Add files via upload

---
+++ app_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+ThemeData appTheme = ThemeData(
+  
+  scaffoldBackgroundColor: Colors.green,
+  textTheme: TextTheme(
+    bodyText2: TextStyle(
+      fontSize: 30,
+      color: Colors.red,
+    ),
+  ),
+  cardTheme: CardTheme(),
+);
